31f84cd3b20d2d5fa86ca37ebf1c64d3155f1414
VirginiaAl <32166399+VirginiaAl@users.noreply.github.com>
2017-10-24 16:16:07 +0200

    Update pizza.js

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,56 @@
 // Write your Pizza Builder JavaScript in this file.
+
+//Iteration 1: Add and Remove Toppings
+
+$(document).ready(function() {
+  //Ingredients
+  var greenPepper = $('.green-pepper');
+  var pepperoni = $('.pep');
+  var mushroom = $('.mushroom');
+  var sauce = $('.sauce');
+  var sauceWhite = ('sauce-white');
+  var glutenFree = ('crust-gluten-free');
+  var crust = $('.crust');
+
+
+  //buttons
+  var btnGreenPepers = $('.btn-green-peppers');
+  var btnPepperonni = $('.btn-pepperonni');
+  var btnMushrooms = $('.btn-mushrooms');
+  var btnSauce = $('.btn-sauce');
+  var btnCrust = $('.btn-crust');
+
+  //Adding ingredients
+
+  btnGreenPepers.click(function() {
+    greenPepper.toggle();
+  });
+
+  btnPepperonni.click(function() {
+    pepperoni.toggle();
+  });
+
+  btnMushrooms.click(function() {
+    mushroom.toggle();
+  });
+
+  //default pizza ingredients
+
+  crust.removeClass(glutenFree);
+  sauce.removeClass(sauceWhite);
+
+  //extra pizza ingredients
+
+  btnSauce.click(function() {
+    sauce.toggleClass(sauceWhite);
+  });
+
+  btnCrust.click(function() {
+    crust.toggleClass(glutenFree);
+  });
+
+  //
+
+
+
+});
